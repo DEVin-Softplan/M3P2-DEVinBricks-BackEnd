84d93f5b922e7159f7e13a67ae8a2a335ec67777
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-07-19 23:41:11 -0300

    Adiciona atributos entidade Usuario.

--- DEVinBricks/Models/Usuario.cs
+++ DEVinBricks/Models/Usuario.cs
@@ -5,6 +5,16 @@ namespace DEVinBricks.Models
 {
     public partial class Usuario : Log
     {
-       
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public bool Admin { get; set; }
+        public bool Ativo { get; set; }
+
+        public Usuario()
+        {
+        }
     }
 }
