a1378ffc82b27df9266c86a263a2541bca739b3a
Narriman Lima <narrimancslima@gmail.com>
2022-07-20 16:38:41 -0300

    Interface método ObterPorLoginEsENHA

--- DEVinBricks/Repositories/IUsuarioRepository.cs
+++ DEVinBricks/Repositories/IUsuarioRepository.cs
@@ -1,10 +1,14 @@
-﻿using DEVinBricks.Models;
+﻿using DEVinBricks.DTO;
+using DEVinBricks.Models;
 
 namespace DEVinBricks.Repositories
 {
     public interface IUsuarioRepository
     {
         public List<Usuario> listarUsuarios();
+
+        public Usuario ObterPorLoginESenha(string login, string senha);
+
     }
 
 }
