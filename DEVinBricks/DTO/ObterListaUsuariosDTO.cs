711d9096d67081bfdbceb06782316f5b7bc265f7
Romulo-Ladeira <93687924+Romulo-Ladeira@users.noreply.github.com>
2022-07-20 17:51:49 -0300

    feat/Cria ObterListaUsuariosDTO

---
+++ DEVinBricks/DTO/ObterListaUsuariosDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DEVinBricks.DTO
+{
+    public class ObterListaUsuariosDTO
+    {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+    }
+}
