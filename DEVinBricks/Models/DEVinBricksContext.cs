11fa8baacb30dd4bbf8250196a6270d693661d24
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-07-19 22:48:26 -0300

    Corrige namespace models.

--- DEVinBricks/Models/DEVinBricksContext.cs
+++ DEVinBricks/Models/DEVinBricksContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace WebApplication1.Models
+namespace DEVinBricks.Models
 {
     public partial class DEVinBricksContext : DbContext
     {
