67a23f923a3dc61aee252e32d80d70a777354cd4
GuiVeigaSesiSenai <guilherme_v_pinto@estudante.sesisenai.org.br>
2022-07-21 21:49:59 -0300

    Cria a Tabela de Fretes

--- DEVinBricks/Migrations/20220722003854_CriaTabelaFrete.Designer.cs
+++ DEVinBricks/Migrations/20220722003854_CriaTabelaFrete.Designer.cs
@@ -4,6 +4,7 @@ using DEVinBricks.Repositories.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DEVinBricks.Migrations
 {
     [DbContext(typeof(DEVinBricksContext))]
-    partial class DEVinBricksContextModelSnapshot : ModelSnapshot
+    [Migration("20220722003854_CriaTabelaFrete")]
+    partial class CriaTabelaFrete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DEVinBricks/Repositories/Models/Frete.cs
+++ DEVinBricks/Repositories/Models/Frete.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DEVinBricks.Repositories.Models
 {
     public partial class Frete : BaseEntity
     {
-        
+        public string Cep { get; set; }
+        public int EstadoId { get; set; }
+        public string Cidade { get; set; }
+        public string Logadouro { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
     }
 }
