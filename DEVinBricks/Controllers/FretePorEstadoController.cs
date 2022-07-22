ec1c5d41e7f157e07df755a1d795eda700aaeafd
zighue1 <federico_robillard@estudante.sesisenai.org.br>
2022-07-21 22:19:34 -0300

    hotfix controller valorfreteporestado controller

--- DEVinBricks/Controllers/FretePorEstadoController.cs
+++ DEVinBricks/Controllers/FretePorEstadoController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using DEVinBricks.Repositories.Models;
 using DEVinBricks.Services.Interfaces;
 using DEVinBricks.DTO;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DEVinBricks.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    
     public class FretePorEstadoController : ControllerBase
     {
         private readonly IValorFretePorEstadoService _service;
+   
         public FretePorEstadoController(IValorFretePorEstadoService service)
         {
+
             _service = service;
         }
 
@@ -20,17 +25,20 @@ namespace DEVinBricks.Controllers
         /// <summary>
         /// Edita o valor do Frete por Estado
         /// </summary>
-        /// <returns>Token de Autenticação</returns>
-        /// <response code="201">Login efetuado com sucesso.</response>
-        /// <response code="404">Login e/ou senha incorreto(s).</response>
-       
-        [ProducesResponseType(StatusCodes.Status201Created)]
+        /// <returns>Valor do Frete por Estado Atualizado</returns>
+        /// <response code="200">Valor do Frete por Estado atualizado com sucesso</response>
+        /// <response code="404">Id não encontrado no database</response>
+        /// <response code="400">Dados Inconsistentes, verifique o Id</response>
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpPut]
-       [Route("editar/{id}")]
+        [Authorize]
+        [Route("editar/{id}")]
        public IActionResult Editar([FromBody] ValorFretePorEstadoDTO dto, int id)
         {
+            var teste = User.Identity.Name;
             if (dto.Id != id)
                 return BadRequest("Dados inconsistentes");
 
