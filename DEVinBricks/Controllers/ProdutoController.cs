aef01f5477e9573e59a08b079f084fef7b46fe5e
Vinicius Hiago da Rosa <93688147+viniHiagoRosa@users.noreply.github.com>
2022-07-25 19:51:19 -0300

    fix repositort

--- DEVinBricks/Controllers/ProdutoController.cs
+++ DEVinBricks/Controllers/ProdutoController.cs
@@ -24,6 +24,7 @@ namespace DEVinBricks.Controllers
         {
             try
             {
+                
                 var response = _service.ObterProdutoPorId(id);
                 return Ok(response);
             }
