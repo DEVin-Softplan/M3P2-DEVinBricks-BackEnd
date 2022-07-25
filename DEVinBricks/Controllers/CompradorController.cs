9490a423b820d23d0aecda8a15234fe51f9b9250
rmyght <ramon.stelles@gmail.com>
2022-07-25 15:58:45 -0300

    Adiciona Autorização Admin no GetComprador.

--- DEVinBricks/Controllers/CompradorController.cs
+++ DEVinBricks/Controllers/CompradorController.cs
@@ -1,6 +1,7 @@
 ﻿using DEVinBricks.DTO;
 using DEVinBricks.Repositories;
 using DEVinBricks.Repositories.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DEVinBricks.Controllers
@@ -39,10 +40,13 @@ namespace DEVinBricks.Controllers
         /// </summary>
         /// <returns>Lista de Comprador(es) conforme os parâmetros passados</returns>
         /// <response code="200">Lista retornada com sucesso.</response>
+        /// <response code="401">Usuário não autorizado.</response>
         /// <response code="404">Nenhum resultado encontrado.</response>
         [HttpGet(Name = "GetComprador")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = "admin")]
         public ActionResult<Comprador> GetComprador(string? nome, string? cpf, int pagina = 0, int tamanhopagina = 10)
         {
             CompradorGetDTO comprador = CompradorGetDTO.ConverterParaEntidadeCompradorGetDTO(nome, cpf, pagina, tamanhopagina);
