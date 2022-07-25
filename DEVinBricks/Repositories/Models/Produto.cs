d7e0b2145771e80f5111752d95e310449b9cc85a
rmyght <ramon.stelles@gmail.com>
2022-07-25 16:47:14 -0300

    Remove Id do Model Produto.

--- DEVinBricks/Repositories/Models/Produto.cs
+++ DEVinBricks/Repositories/Models/Produto.cs
@@ -2,7 +2,6 @@
 {
     public partial class Produto : BaseEntity
     {
-        public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
