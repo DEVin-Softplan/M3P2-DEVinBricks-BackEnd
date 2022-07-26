587b32264b5be8f19aa6e38521fa53b4cc076ea6
zighue1 <federico_robillard@estudante.sesisenai.org.br>
2022-07-25 21:07:03 -0300

    ajusta id duplicado

--- DEVinBricks/Repositories/Models/Comprador.cs
+++ DEVinBricks/Repositories/Models/Comprador.cs
@@ -2,7 +2,6 @@
 {
     public partial class Comprador : BaseEntity
     {
-        public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
