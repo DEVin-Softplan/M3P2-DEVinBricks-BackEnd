24c79474ebf01fe528b94b249fe14f21746ec314
zighue1 <federico_robillard@estudante.sesisenai.org.br>
2022-07-27 20:13:58 -0300

    fix migrations

--- DEVinBricks/Repositories/Models/Venda.cs
+++ DEVinBricks/Repositories/Models/Venda.cs
@@ -5,9 +5,9 @@ namespace DEVinBricks.Repositories.Models
 {
     public partial class Venda : BaseEntity
     {
-        public int Id { get; set; }
+     
         public Comprador CompradorId { get; set; }
         public Usuario VendedorId { get; set; }
-        public Frete FreteId { get; set; }
+        public FreteModel FreteId { get; set; }
     }
 }
