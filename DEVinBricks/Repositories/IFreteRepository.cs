9dda8ea484ce98b13d5f2745043fb35431967ee3
GuiVeigaSesiSenai <guilherme_v_pinto@estudante.sesisenai.org.br>
2022-07-28 15:51:45 -0300

    Implementa o endpoint Get By Id em Frete Adiciona a classe FreteRepository Adiciona a interface IFreteRepository Adiciona a injeção de dependência de FreteRepository em Program

---
+++ DEVinBricks/Repositories/IFreteRepository.cs
@@ -0,0 +1,11 @@
+﻿using DEVinBricks.DTO;
+using DEVinBricks.Repositories.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEVinBricks.Repositories
+{
+    public interface IFreteRepository
+    {
+        public FreteModel ObterFretePorId(int id);
+    }
+}
