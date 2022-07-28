913088dc743ef57a0638e4d0ad5d50bf35c8d8f9
douglas-devinhouse <douglas_nascimento4@estudante.sesisenai.org.br>
2022-07-28 20:17:56 -0300

    feature/Romeu-Cria-IVendaService

---
+++ DEVinBricks/Services/Interfaces/IVendaService.cs
@@ -0,0 +1,10 @@
+﻿using DEVinBricks.DTO;
+using DEVinBricks.Repositories.Models;
+
+namespace DEVinBricks.Services.Interfaces
+{
+    public interface IVendaService
+    {
+        Task<int> CadastrarVenda(int idProduto, int quantidadeProduto, int idComprador, int idVendedor);        
+    }
+}
