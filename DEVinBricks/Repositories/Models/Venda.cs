95d6baa7b6d76bfd5db8e75046ea793d0d5e07bc
VERONI JOSE VIEIRA JUNIOR <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-07-29 23:10:16 -0300

    corrifido alteração indevida Classe Venda

--- DEVinBricks/Repositories/Models/Venda.cs
+++ DEVinBricks/Repositories/Models/Venda.cs
@@ -6,8 +6,8 @@ namespace DEVinBricks.Repositories.Models
     public partial class Venda : BaseEntity
     {
      
-        public int CompradorId { get; set; }
-        public int VendedorId { get; set; }
+        public Comprador CompradorId { get; set; }
+        public Venda VendedorId { get; set; }
         public FreteModel FreteId { get; set; }
 
     }
