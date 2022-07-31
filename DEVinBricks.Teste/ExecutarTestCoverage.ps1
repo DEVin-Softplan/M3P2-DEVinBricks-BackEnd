f0bb8e915c352e815be44ac661d7c4b826808e76
rmyght <ramon.stelles@gmail.com>
2022-07-31 15:06:12 -0300

    Adiciona Script para Executar o TestCoverage de forma automática.

---
+++ DEVinBricks.Teste/ExecutarTestCoverage.ps1
@@ -0,0 +1,9 @@
+dotnet test --collect:"XPlat Code Coverage"
+cd .\TestResults
+cd (gci * | sort LastWriteTime |select -last 1).Name
+$ReportName = "-reports:coverage.cobertura.xml"
+$TargetDir = "-targetdir:TestCoverageReport"
+$ClassFilters = "-classfilters:-DEVinBricks.Migrations.*;-DEVinBricks.Repositories.Models.*;-DEVinBricks.Seeds.*;-DEVinBricks.Settings;-DEVinBricks.Controllers.Validacoes.*;-Program"
+reportgenerator $ReportName $TargetDir $ClassFilters
+TestCoverageReport/index.html
+cd ../..
